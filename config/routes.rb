e0a4e916ffd01af024cc03f79ea6d70457d1298b
Elunaer <hothairyorkshire@gmail.com>
2021-04-13 21:14:36 +0100

    Added the articles page using rails scaffold and some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'home', to: 'pages#home'
+  get 'articles', to: 'articles#index'
 end
